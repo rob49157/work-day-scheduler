fe8f7ba410bc6542fd5915015203355812aa7b53
roberto <robertopaipo@hotmail.com>
2021-09-28 06:54:33 -0400

    trying different colors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var row = document.getElementById("#row")
-var timecolor= document.getElementsByClassName(".table-active")
+// var row = document.getElementById("#row")
+// var timecolor= document.getElementsByClassName(".table-active")
  
 //add local time/ date
 
@@ -7,25 +7,30 @@ var today = moment()
 setInterval(() => {
     var time= moment().format("MMM Do, YYYY HH:mm:ss ")
     $("#time").text(time)
+    var timestring= '6:53:00'
+    if (timestring === moment() ){
+        $('.tablea-active').css('color','white')
+    } 
     
 }, 1000)
 
 // change row colors.run a loop thru the today function, and everytime the time changes change  the color of the row using the ID"ROW" TO CHANGE THE TABLE ACTIVE CLASS. WITH AND IF STATEMENT. 
 
-function color(){
-    if (timecolor === moment() ){
-        color : white
-    } 
-    else if( timecolor >= moment()){
-        color: red
+// function color(){
+//     var timestring= '9:00:00'
+//     if (timestring === moment() ){
+//         $('.tablea-active').css('color','white')
+//     } 
+//     else if( timestring >= moment()){
+//        $('.table-active').css('color','green')
         
-    }
-    else if( timecolor <= moment()){
-        color : green
-    }
+//     }
+//     else if( timestring <= moment()){
+//         $('.table-active').css('color','red')
+//     }
     
     
-}
+// }
 
 // add to localstorage
 function save(text){
