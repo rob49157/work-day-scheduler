614442fed17ed0a7c0475528fcee7bfa80edb6f0
rob49157 <robertopaipo@hotmail.com>
2021-09-28 16:38:18 -0400

    color changed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // var row = document.getElementById("#row")
 // var timecolor= document.getElementsByClassName(".table-active")
- 
+
+
+
+
 //add local time/ date
 
 var today = moment()
@@ -8,29 +11,34 @@ setInterval(() => {
     var time= moment().format("MMM Do, YYYY HH:mm:ss ")
     $("#time").text(time)
     var timestring= '6:53:00'
-    if (timestring === moment() ){
-        $('.tablea-active').css('color','white')
-    } 
-    
-}, 1000)
+    }, 1000)
 
 // change row colors.run a loop thru the today function, and everytime the time changes change  the color of the row using the ID"ROW" TO CHANGE THE TABLE ACTIVE CLASS. WITH AND IF STATEMENT. 
 
-// function color(){
-//     var timestring= '9:00:00'
-//     if (timestring === moment() ){
-//         $('.tablea-active').css('color','white')
-//     } 
-//     else if( timestring >= moment()){
-//        $('.table-active').css('color','green')
-        
-//     }
-//     else if( timestring <= moment()){
-//         $('.table-active').css('color','red')
-//     }
+
+
+var hour= moment().format()
+
+
+
+
+$('tr.table-active').each(function(){
+    var data = $(this).find('th').html()
+    var dataNumber = parseInt(data)
+    var hour = moment().format('HH')
+    var hourNumber = parseInt(hour)
+
+    if(hourNumber > dataNumber){
+        $(this).css("background", "red")
+    }
+    else
+    $(this).css("background", "green")
     
     
-// }
+})
+
+
+
 
 // add to localstorage
 function save(text){
